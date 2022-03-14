5b0d3501b1cf74e087feaeeeb08b7f385e4b05b3
aGautam <aradhanagautam103@gmail.com>
2022-03-15 00:11:15 +0530

    Add Power A file

---
+++ src/main/java/Power_A.java
@@ -0,0 +1,25 @@
+package javaProgram;
+
+import java.util.Scanner;
+
+public class Power_A {
+
+	public static void main(String[] args) {
+		int base;
+		int power;
+
+		System.out.println("ENTER NUMBER:");
+		System.out.println("ENTER POWER:");
+
+		Scanner s = new Scanner(System.in);
+		base = s.nextInt();
+		power = s.nextInt();
+		int num = 1;
+		
+		for (int i = 1; i <= power; i++) {
+			num*= base; 
+		}
+		System.out.println(num);
+	}
+
+}
